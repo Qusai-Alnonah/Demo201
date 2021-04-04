90eb596820afcb3dcb17ccabac729021f9bd82eb
Qusai-Alnonah <kusaialnona@yahoo.com>
2021-04-04 12:38:09 +0300

    add demo

---
+++ HTML/js/app.js
@@ -0,0 +1,35 @@
+'use strict';
+var x;
+x= 10;
+userName = "Bashar";
+
+var userName= "bshar";
+
+let userName = "bashar" 
+
+let userName = "mahmoud";
+
+if(5>2){
+    let x = 5; 
+}
+console.log(x);
+
+if(5>2){
+    let x = 5;
+    console.log(x) 
+}
+
+// always use let insted var
+
+const url = "https://www.google.jo";
+url =10 ; //cant do that
+let anything = "Hello"
+for (let i =0 ;i<5 ;i++){
+    console.log(i);
+
+ console.log(anything);
+}
+
+let userAge = prompt('what\'s your age')
+
+let goodNow =prompt('what are you doing now');
